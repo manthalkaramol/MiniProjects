e88aa5ebcd6ff4be36b0f2bd693c4c55d88e9001
Amol Manthalkar <manthalkaramol@gmail.com>
2021-03-02 18:48:34 +0530

    Adding a python file that has API get call

---
+++ API/api.py
@@ -0,0 +1,25 @@
+import flask
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+# Create some test data for our catalog in the form of a list of dictionaries.
+data = {
+    "message": "Hello Deserve",
+    "branches": "US, Pune, Bangalore",
+    "containername": "Deserve"
+}
+
+
+@app.route('/')
+def home():
+    return '''<h1>Welcome to Deserve!</h1>'''
+
+
+# A route to return all of the available entries in our catalog.
+@app.route('/api/get', methods=['GET'])
+def api_all():
+    return jsonify(data)
+
+app.run()
